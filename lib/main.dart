8e4776e44ee4faf09304d0fb3e959e4f2388ceae
FlutLab.io <>
2022-05-02 19:02:56 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -49,9 +49,8 @@ class _MyHomePageState extends State<MyHomePage> {
     "assets/images/img6.jpg",
     "assets/images/img5.jpg",
     "assets/images/img4.jpg",
-    "assets/images/img3.jpg",
-    "assets/images/img2.jpg",
     "assets/images/img1.jpg",
+    "assets/images/img2.jpg",
   ];
   @override
   Widget build(BuildContext context) {

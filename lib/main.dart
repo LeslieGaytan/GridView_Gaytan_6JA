e5e8616f29ab49a91762ed8492de1feff1003c62
FlutLab.io <>
2022-05-02 19:23:32 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:gaytan/gridviewempleado.dart';
 
-class PaginaInicial extends StatefulWidget {
-  PaginaInicial({Key? key}) : super(key: key);
-  @override
-  _PaginaInicialState createState() => _PaginaInicialState();
-} //widget con estado
+void main() {
+  runApp(DominosApp());
+} //funcion principal
 
-class _PaginaInicialState extends State<PaginaInicial> {
-  List<String> images = [
-    "assets/images/img1.jpg",
-    "assets/images/img2.jpg",
-    "assets/images/img3.jpg",
-    "assets/images/img4.jpg",
-    "assets/images/img5.jpg",
-    "assets/images/img6.jpg",
-    "assets/images/img3.jpg",
-    "assets/images/img6.jpg",
-    "assets/images/img5.jpg",
-    "assets/images/img2.jpg",
-    "assets/images/img5.jpg",
-    "assets/images/img4.jpg",
-    "assets/images/img3.jpg",
-    "assets/images/img2.jpg",
-    "assets/images/img1.jpg",
-    "assets/images/img2.jpg",
-    "assets/images/img3.jpg",
-    "assets/images/img4.jpg",
-    "assets/images/img5.jpg",
-    "assets/images/img6.jpg",
-    "assets/images/img1.jpg",
-    "assets/images/img6.jpg",
-    "assets/images/img5.jpg",
-    "assets/images/img4.jpg",
-    "assets/images/img1.jpg",
-    "assets/images/img2.jpg",
-    "assets/images/img6.jpg",
-  ];
+class DominosApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter GridView"),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Aprendiendo GridView',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
         ),
-        body: GridView.builder(
-          itemCount: images.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Image.asset(images[index], fit: BoxFit.cover);
-          },
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, mainAxisSpacing: 10, crossAxisSpacing: 10),
-          padding: EdgeInsets.all(10),
-          shrinkWrap: true,
-        ));
+        home: PaginaInicial());
   }
-}
+} //widget sin estado
